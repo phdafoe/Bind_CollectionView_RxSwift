afcb18f6ef34a94896e8193b26494d0f3a7072a9
Andres Felipe Ocampo Eljaiesk <aocampoe@everis.com>
2018-03-11 16:44:54 +0100

    Initial Commit

---
+++ Bind_CollectionView_RxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bind_CollectionView_RxSwift
+//
+//  Created by Andres Felipe Ocampo Eljaiesk on 11/3/18.
+//  Copyright © 2018 icologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
